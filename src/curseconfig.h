30386ad7478233f09c004a523fae9ac19b091835
Christian <50080567+ChrisNonyminus@users.noreply.github.com>
2022-08-08 13:32:35 -0600

    Update curseconfig.h

--- src/curseconfig.h
+++ src/curseconfig.h
@@ -4,7 +4,7 @@
 #define CURSE_MATHUTIL 1 // if defined as 1, applies cursed.h to mathutil.c (and forces C_ONLY on) (warning, makes some sounds spammy)
 #define CURSE_BALL     0 // if defined as 1, applies cursed.h to ball.c
 #define CURSE_STCOLI   0 // if defined as 1, applies cursed.h to stcoli.c
-#define CURSE_POLYDISP 1 // if defined as 1, applies cursed.h to polydisp.c
+#define CURSE_POLYDISP 1 // if defined as 1, applies cursed.h to polydisp.c (warning, will cause an unhandled address read error in Dolphin, but you can probably just ignore it)
 
 #define IF_ELSE_RNG       1    // if defined as 1, makes if-statements rng-driven
 #define NO_ELSE           0    // if defined as 1, makes the keyword "else" do nothing
